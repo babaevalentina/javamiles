c1aed57a812f31a392e09ffd05266abf073431f9
Валентина Бабаева <valentina_babaeva@list.ru>
2022-06-23 19:43:20 +0300

    add all

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 6000;
+        int amount = 20;
+        int mile = price / amount;
+
+        System.out.println(mile);
+    }
+}
