790897fd4f5d954bef1ffed0a3b95eadadaa12d0
Валентина Бабаева <valentina_babaeva@list.ru>
2022-06-27 17:07:07 +0300

    Add calculate method

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+        int result = price / 20;
+        System.out.println(result);
+        return result;
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 6000;
-        int amount = 20;
-        int mile = price / amount;
-
-        System.out.println(mile);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
